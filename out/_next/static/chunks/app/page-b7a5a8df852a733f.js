72125afed792222e44d1c2688081214d6ecc21e9
Matt Davey <mattjdavey@outlook.com>
2024-05-04 19:10:51 -0500

    Remove build directory

--- out/_next/static/chunks/app/page-b7a5a8df852a733f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{1650:function(n,e,u){Promise.resolve().then(u.bind(u,8212))},8212:function(n,e,u){"use strict";u.r(e),u.d(e,{default:function(){return r}});var t=u(7437);function r(){return(0,t.jsx)("h1",{children:"Home"})}}},function(n){n.O(0,[971,23,744],function(){return n(n.s=1650)}),_N_E=n.O()}]);
