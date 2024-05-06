5cfcc4ca4cb8482d1c25c2f43d0e72a5d70f3901
Matt Davey <mattjdavey@outlook.com>
2024-05-06 09:40:33 -0500

    Updated some styles.

---
+++ src/app/aws/page.tsx
@@ -0,0 +1,5 @@
+export default function Aws() {
+    return(
+        <h1 className="text-2xl">AWS</h1>
+    )
+}
--- src/app/toolbox/page.tsx
+++ src/app/toolbox/page.tsx
@@ -1,8 +1,9 @@
 export default function Toolbox() {
     return(
         <>
-        <h1>Toolbox</h1>
-        <ul>
+        <h1 className="text-2xl">Toolbox</h1>
+        <hr />
+        <ul className="py-3 list-disc">
             <li>
                 <a href="/toolbox/docker">Docker</a>
             </li>
