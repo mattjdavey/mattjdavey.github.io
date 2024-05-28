19a8f1cfb1e7988963c6abf11b6dcbda5a400455
Matt Davey <mattjdavey@outlook.com>
2024-05-28 17:55:12 -0500

    Update docker command

--- src/app/toolbox/docker/page.tsx
+++ src/app/toolbox/docker/page.tsx
@@ -6,7 +6,7 @@ export default function Docker() {
             <h3 className="py-3">Running PostgreSQL on Docker locally</h3>
             <div className="bg-gray-200 p-3 dark:bg-slate-600">
                 <code>
-                    docker run --name PostgresDb -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=somepassword -d postgres:latest
+                    docker run --name PostgresDb -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -d postgres:latest
                 </code>
             </div>
         </>
